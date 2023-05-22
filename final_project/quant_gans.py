59d7416edce40b207a51420d4c2e7fccb8781a40
Dmitrii <dmitrnv@gmail.com>
2023-05-22 04:35:47 +0300

    fix errors

--- final_project/quant_gans.py
+++ final_project/quant_gans.py
@@ -4,20 +4,76 @@ from torch.nn.utils import weight_norm
 import torch.optim as optim
 import pandas as pd
 import numpy as np
-# import os
+from sklearn.preprocessing import StandardScaler
+
+from gaussianize import *
+import torch.optim as optim
+from tqdm import tqdm
+
+# compute rolling windows from timeseries
+def rolling_window(x, k, sparse=True):
+    
+    out = np.full([k, *x.shape], np.nan)
+    N = len(x)
+    for i in range(k):
+        out[i, :N-i] = x[i:]
+            
+    if not sparse:
+        return out
+
+    return out[:, :-(k-1)]
 
 
+class Loader32():
+    def __init__(self, data, length):
+        assert len(data) >= length
+        self.data = data
+        self.length = length
+    
+    def __getitem__(self, idx):
+        return torch.tensor(self.data[idx:idx+self.length]).reshape(-1, self.length).to(torch.float32)
+        
+    def __len__(self):
+        return max(len(self.data)-self.length, 0)
+
+
+"""
+This is a custom module that trims the input tensor along the time dimension
+(or any dimension specified) to avoid unnecessary computations. It is used in conjunction
+with dilated convolutions to create a causal convolution​
+"""
+class Trim1d(nn.Module):
+    def __init__(self, trim_size):
+        super(Trim1d, self).__init__()
+        self.trim_size = trim_size
+
+    def forward(self, x):
+        return x[:, :, :-self.trim_size].contiguous()
+
+
+"""
+This is the core module in the Temporal Convolutional Network (TCN).
+It includes two dilated causal convolution layers, each followed by a Trim1d module
+(to ensure the output size matches the input size), a ReLU activation, and a dropout layer.
+The entire sequence is wrapped into a PyTorch Sequential model. The block also includes 
+a residual layer, which is used when the number of input channels doesn't match the number 
+of output channels, to ensure the residual connection can be added correctly. 
+The forward method calculates both the output of the sequential model and the residual connection,
+and returns their sum after applying a ReLU activation
+"""
 class TemporalBlock(nn.Module):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
         
         # First convolution layer
-        self.conv1 = nn.Conv1d(n_inputs, n_outputs, kernel_size,
-                               stride=stride, dilation=dilation, padding=padding)
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                               stride=stride, dilation=dilation, padding=padding))
         
         # Second convolution layer
-        self.conv2 = nn.Conv1d(n_outputs, n_outputs, kernel_size,
-                               stride=stride, dilation=dilation, padding=padding)
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                               stride=stride, dilation=dilation, padding=padding))
+        
+        self.trim = Trim1d(padding)
         
         # Activation function
         self.relu = nn.ReLU()
@@ -25,142 +81,146 @@ class TemporalBlock(nn.Module):
         # Dropout layer for regularization
         self.dropout = nn.Dropout(dropout)
         
+        # self.net = nn.Sequential(self.conv1, self.relu, self.dropout, self.conv2, self.relu, self.dropout)
+        if padding == 0:
+            self.net = nn.Sequential(self.conv1, self.relu, self.dropout, self.conv2, self.relu, self.dropout)
+        else:
+            self.net = nn.Sequential(self.conv1, self.trim, self.relu, self.dropout, self.conv2, self.trim, self.relu, self.dropout)
+
         # Residual connection
         self.residual = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.init_weights()
 
-    def forward(self, x):
-        # Store the input as residual
-        residual = x
-        
-        # Apply first convolutional layer, ReLU activation, and dropout
-        out = self.relu(self.conv1(x))
-        out = self.dropout(out)
-        
-        # Apply second convolutional layer, ReLU activation, and dropout
-        if out.shape[2] == 1:
-            # self.conv2.kernel_size = (1,)
-            test = 1
-        
-        out = self.relu(self.conv2(out))
-        out = self.dropout(out)
-        
-        # If the residual connection is needed, apply it
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.5)
+        self.conv2.weight.data.normal_(0, 0.5)
         if self.residual is not None:
-            residual = self.residual(x)
-        
-        # Add the residual back to the output
-        return out + residual
-
-
-class Generator(nn.Module):
-    def __init__(self, n_layers, n_channels, n_input, n_output, kernel_size, stride, dilation, padding, dropout):
-        super(Generator, self).__init__()
-        layers = []
-        for i in range(n_layers):
-            dilation_size = dilation ** i
-            in_channels = n_input if i == 0 else n_channels
-            out_channels = n_channels
-            layers.append(
-                TemporalBlock(in_channels, out_channels, kernel_size, stride, dilation_size, padding, dropout)
-            )
-        self.network = nn.Sequential(*layers)
-        self.final_conv = nn.Conv1d(n_channels, n_output, kernel_size=1)
+            self.residual.weight.data.normal_(0, 0.5)
 
     def forward(self, x):
-        out = self.network(x)
-        out = self.final_conv(out)
-        return out
+        out = self.net(x)
+        residual = x if self.residual is None else self.residual(x)
+        return out, self.relu(out + residual)
+
+"""
+Generator and Discriminator classes: These are the main parts of the model.
+Both of them use a sequence of TemporalBlocks followed by a final Trim1d layer.
+In the forward method, they calculate the output for each TemporalBlock, storing
+the 'skip' outputs in a list. Then, they add the final output to the sum of the skip outputs
+and pass the result through the final Trim1d layer. The generator has a sequence 
+of TemporalBlocks with varying dilations and paddings, while the discriminator has 
+a fixed dilation and padding for all TemporalBlocks
+"""
+class Generator(nn.Module):
+    def __init__(self):        
+        super(Generator, self).__init__()
+        self.network = nn.ModuleList([TemporalBlock(3, 80, kernel_size=1, stride=1, dilation=1, padding=0),
+                                 *[TemporalBlock(80, 80, kernel_size=2, stride=1, dilation=i, padding=i) for i in [1, 2, 4, 8, 16, 32]]])
+        self.final_conv = nn.Conv1d(80, 1, kernel_size=1, stride=1, dilation=1)
+
+    def forward(self, x):
+        skip_layers = []
+        for layer in self.network:
+            skip, x = layer(x)
+            skip_layers.append(skip)
+        x = self.final_conv(x + sum(skip_layers))
+        return x
+    
 
 class Discriminator(nn.Module):
-    def __init__(self, n_layers, n_channels, n_input, n_output, kernel_size, stride, dilation, padding, dropout):
+    def __init__(self, seq_len, conv_dropout=0.05):
         super(Discriminator, self).__init__()
-        layers = []
-        for i in range(n_layers):
-            dilation_size = dilation ** i
-            in_channels = n_input if i == 0 else n_channels
-            out_channels = n_channels
-            layers.append(
-                TemporalBlock(in_channels, out_channels, kernel_size, stride, dilation_size, padding, dropout)
-            )
-        self.network = nn.Sequential(*layers)
-        self.final_conv = nn.Conv1d(n_channels, n_output, 1)
+        self.seq_len = seq_len
+        self.network = nn.ModuleList([TemporalBlock(1, 80, kernel_size=1, stride=1, dilation=1, padding=0),
+                                 *[TemporalBlock(80, 80, kernel_size=2, stride=1, dilation=i, padding=i) for i in [1, 2, 4, 8, 16, 32]]])
+        self.final_conv = nn.Conv1d(80, 1, kernel_size=1, dilation=1)
+        self.to_prob = nn.Sequential(nn.Linear(seq_len, 1), nn.Sigmoid())
 
     def forward(self, x):
-        out = self.network(x)
-        out = self.final_conv(out)
-        return out	
-
-def load_financial_data(filename):
-	data = pd.read_csv(filename)
-	adj_close_prices = data['Adj Close'].dropna()
-	normalized_prices = adj_close_prices / adj_close_prices.iloc[0] - 1
-	data_array = normalized_prices.values.reshape(-1, 1)
-	return data_array
-
-def train_quant_gans(data, generator, discriminator, num_epochs, batch_size, device):
-	criterion = nn.BCEWithLogitsLoss()
-	generator_optimizer = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-	discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))	
-
-	real_labels = torch.ones(batch_size, 1).to(device)
-	fake_labels = torch.zeros(batch_size, 1).to(device)
-	fixed_noise = torch.randn(batch_size, 1, 100).to(device)
-
-	for epoch in range(num_epochs):
-		for i in range(0, len(data), batch_size):
-			# Train the discriminator on real data
-			real_data = torch.FloatTensor(data[i:i + batch_size]).unsqueeze(1).to(device)
-			real_output = discriminator(real_data)
-			real_loss = criterion(real_output, real_labels)
-			real_score = torch.sigmoid(real_output)
-
-			# Train the discriminator on fake data
-			noise = torch.randn(batch_size, 1, 100).to(device)
-			fake_data = generator(noise)
-			fake_output = discriminator(fake_data.detach())
-			fake_loss = criterion(fake_output, fake_labels)
-			fake_score = torch.sigmoid(fake_output)
-
-			# Optimize the discriminator
-			discriminator_loss = real_loss + fake_loss
-			discriminator_optimizer.zero_grad()
-			discriminator_loss.backward()
-			discriminator_optimizer.step()
-
-			# Train the generator
-			fake_output = discriminator(fake_data)
-			generator_loss = criterion(fake_output, real_labels)
-
-			# Optimize the generator
-			generator_optimizer.zero_grad()
-			generator_loss.backward()
-			generator_optimizer.step()
-
-		print(f"Epoch [{epoch + 1}/{num_epochs}] - D Loss: {discriminator_loss.item()} - G Loss: {generator_loss.item()}")
+        skip_layers = []
+        for layer in self.network:
+            skip, x = layer(x)
+            skip_layers.append(skip)
+        x = self.final_conv(x + sum(skip_layers))
+        if x.size(0) != self.seq_len:
+            target = torch.zeros(self.seq_len, 1, 1)
+            target[:x.size()[0], :x.size()[1], :x.size()[2]] = x
+            x = target
+        return self.to_prob(x.T).squeeze()
 
 
 def main():
-	data = load_financial_data('AAPL.csv')
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-	
-	n_layers = 3
-	n_channels = 11
-	n_input = 1
-	n_output = 1
-	kernel_size = 2
-	stride = 1
-	dilation = 2
-	padding = 1
-	dropout = 0.2
-	num_epochs = 200
-	batch_size = 64
+    relative_path = './final_project/'
+    data_path = 'AAPL.csv'
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-	generator = Generator(n_layers, n_channels, n_input, n_output, kernel_size, stride, dilation, padding, dropout).to(device)
-	discriminator = Discriminator(n_layers, n_channels, n_input, n_output, kernel_size, stride, dilation, padding, dropout).to(device)
+    def dateparse(d):
+        return pd.Timestamp(d)
 
-	train_quant_gans(data, generator, discriminator, num_epochs, batch_size, device)
+    data = pd.read_csv(relative_path + data_path, parse_dates={'datetime': ['Date']}, date_parser=dateparse)
+    df = data['Close']
+
+    returns = df.shift(1)/df - 1
+    log_returns = np.log(df/df.shift(1))[1:].to_numpy().reshape(-1, 1)
+    standardScaler1 = StandardScaler()
+    standardScaler2 = StandardScaler()
+    gaussianize = Gaussianize()
+    log_returns_preprocessed = standardScaler2.fit_transform(gaussianize.fit_transform(standardScaler1.fit_transform(log_returns)))
+
+    num_epochs = 100
+    nz = 3
+    batch_size = 80
+    seq_len = 80
+    clip= 0.01
+    lr = 0.0002
+    generator_path = f'./trained/'
+    file_name = 'AAPL_daily'
+
+    generator = Generator().to(device)
+
+    train = False
+
+    if train:
+        discriminator = Discriminator(seq_len).to(device)
+        disc_optimizer = optim.RMSprop(discriminator.parameters(), lr=lr)
+        gen_optimizer = optim.RMSprop(generator.parameters(), lr=lr)
+
+        dataset = Loader32(log_returns_preprocessed, 127)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+        dataset = Loader32(log_returns_preprocessed, 1)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+        t = tqdm(range(num_epochs))
+        for epoch in t:
+            for idx, data in enumerate(dataloader, 0):
+
+                discriminator.zero_grad()
+                real = data.to(device)
+                batch_size, seq_len = real.size(0), real.size(1)
+                noise = torch.randn(batch_size, nz, seq_len, device=device)
+                fake = generator(noise).detach()
+                disc_loss = -torch.mean(discriminator(real)) + torch.mean(discriminator(fake))
+                disc_loss.backward()
+                disc_optimizer.step()
+
+                for dp in discriminator.parameters():
+                    dp.data.clamp_(-clip, clip)
+        
+                if idx % 5 == 0:
+                    generator.zero_grad()
+                    gen_loss = -torch.mean(discriminator(generator(noise)))
+                    gen_loss.backward()
+                    gen_optimizer.step()            
+            t.set_description('Discriminator Loss: %.8f Generator Loss: %.8f' % (disc_loss.item(), gen_loss.item()))
+                
+        # Save
+        torch.save(generator, f'{relative_path + generator_path}trained_generator_{file_name}_epoch_{epoch}.pth')
+
+    else:
+        # Load
+        generator = torch.load(f'{relative_path + generator_path}trained_generator_{file_name}_epoch_{num_epochs-1}.pth')
+        generator.eval() 
 
 
 if __name__ == "__main__":
-	main()
+    main()
 	
--- final_project/readme.md
+++ final_project/readme.md
@@ -10,7 +10,7 @@ To train the QuantGANs model, follow these steps:
 1. Prepare the financial data by downloading a historical stock prices CSV file from Yahoo Finance. In this example, we use the adjusted closing prices of the Apple Inc. (AAPL) stock.
 2. Load the financial data using the load_financial_data function.
 3. Initialize the generator and the discriminator.
-4. Train the QuantGANs model using the train_quant_gans function with the loaded data, generator, and discriminator.
+4. Train the QuantGANs model using the example from the example.ipynb.
 
 ## Using the Model in a Jupyter Notebook
 You can use the QuantGANs model in a Jupyter Notebook by importing the necessary classes and functions from the provided code. Follow these steps:
@@ -18,7 +18,7 @@ You can use the QuantGANs model in a Jupyter Notebook by importing the necessary
 1. Import the required classes and functions from the provided code.
 2. Prepare the financial data as described in the "Training the Model" section above.
 3. Initialize the generator and the discriminator.
-4. Train the QuantGANs model using the train_quant_gans function.
+4. Train the QuantGANs model using the the example from the example.ipynb function.
 5. After training, you can use the generator to create synthetic financial time series data by providing random noise as input.
 
 ## Getting Stock Data from Yahoo Finance
@@ -34,40 +34,119 @@ To download historical stock prices from Yahoo Finance:
 Here's a quick example of how to use the QuantGANs model in a Jupyter Notebook:
 
 ```python
-import torch
-from quant_gans import Generator, Discriminator, load_financial_data, train_quant_gans
-
-# Load financial data
-data = load_financial_data('AAPL.csv')
-
-# Initialize the generator and the discriminator
+data_path = 'AAPL.csv'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-generator = Generator(n_layers=8, n_channels=64, n_input=1, n_output=1, kernel_size=2, stride=1, dilation=2, padding=1, dropout=0.2).to(device)
-discriminator = Discriminator(n_layers=8, n_channels=64, n_input=1, n_output=1, kernel_size=2, stride=1, dilation=2, padding=1, dropout=0.2).to(device)
 
-# Train the QuantGANs model
-train_quant_gans(data, generator, discriminator, num_epochs=200, batch_size=64, device=device)
+def dateparse(d):
+    return pd.Timestamp(d)
 
-#After training, you can use the generator to create synthetic financial time series data:
-noise = torch.randn(batch_size, 1, 100).to(device)
-generated_data = generator(noise)
+# Preprossesing
+data = pd.read_csv(data_path, parse_dates={'datetime': ['Date']}, date_parser=dateparse)
+df = data['Close']
+
+returns = df.shift(1)/df - 1
+log_returns = np.log(df/df.shift(1))[1:].to_numpy().reshape(-1, 1)
+standardScaler1 = StandardScaler()
+standardScaler2 = StandardScaler()
+gaussianize = Gaussianize()
+log_returns_preprocessed = standardScaler2.fit_transform(gaussianize.fit_transform(standardScaler1.fit_transform(log_returns)))
+
+num_epochs = 100
+nz = 3
+batch_size = 80
+seq_len = 80
+clip= 0.01
+lr = 0.0002
+generator_path = f'./trained/'
+file_name = 'AAPL_daily'
+
+generator = Generator().to(device)
+
+train = True
+
+if train:
+    discriminator = Discriminator(seq_len).to(device)
+    disc_optimizer = optim.RMSprop(discriminator.parameters(), lr=lr)
+    gen_optimizer = optim.RMSprop(generator.parameters(), lr=lr)
+
+    dataset = Loader32(log_returns_preprocessed, 127)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+    dataset = Loader32(log_returns_preprocessed, 1)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+    t = tqdm(range(num_epochs))
+    for epoch in t:
+        for idx, data in enumerate(dataloader, 0):
+
+            discriminator.zero_grad()
+            real = data.to(device)
+            batch_size, seq_len = real.size(0), real.size(1)
+            noise = torch.randn(batch_size, nz, seq_len, device=device)
+            fake = generator(noise).detach()
+            disc_loss = -torch.mean(discriminator(real)) + torch.mean(discriminator(fake))
+            disc_loss.backward()
+            disc_optimizer.step()
+
+            for dp in discriminator.parameters():
+                dp.data.clamp_(-clip, clip)
+    
+            if idx % 5 == 0:
+                generator.zero_grad()
+                gen_loss = -torch.mean(discriminator(generator(noise)))
+                gen_loss.backward()
+                gen_optimizer.step()            
+        t.set_description('Discriminator Loss: %.8f Generator Loss: %.8f' % (disc_loss.item(), gen_loss.item()))
+            
+    # Save
+    torch.save(generator, f'{generator_path}trained_generator_{file_name}_epoch_{epoch}.pth')
+
+else:
+    # Load
+    generator = torch.load(f'{generator_path}trained_generator_{file_name}_epoch_{num_epochs-1}.pth')
+    generator.eval() 
+
+noise = torch.randn(80,3,127).to(device)
+y = generator(noise).cpu().detach().squeeze();
+
+y = (y - y.mean(axis=0))/y.std(axis=0)
+y = standardScaler2.inverse_transform(y)
+y = np.array([gaussianize.inverse_transform(np.expand_dims(x, 1)) for x in y]).squeeze()
+y = standardScaler1.inverse_transform(y)
+
+#filtering
+y = y[(y.max(axis=1) <= 2 * log_returns.max()) & (y.min(axis=1) >= 2 * log_returns.min())]
+y -= y.mean()
+len(y)    
 ```
 
 ## Visualizing Generated Time Series Data
 After generating synthetic financial time series data using the trained QuantGANs model, you can visualize the data using Python libraries like Matplotlib or Plotly. Here's an example using Matplotlib:
 
 ```python
-import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(16,9))
+ax.plot(np.cumsum(y[0:30], axis=1).T)
+ax.set_title('30 generated log graphs'.format(len(y)))
+ax.set_xlabel('days')
+ax.set_ylabel('Cumalative log');
 
-# Generate synthetic financial time series data
-noise = torch.randn(1, 1, 100).to(device)
-generated_data = generator(noise).detach().cpu().numpy().flatten()
+windows = [1, 5, 20, 100]
 
-# Plot the generated data
-plt.plot(generated_data)
-plt.title("Generated Financial Time Series Data")
-plt.xlabel("Time")
-plt.ylabel("Normalized Adjusted Close Price")
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(20, 10))
+
+
+for i in range(len(windows)):
+    row = min(max(0, i-1), 1)
+    col = i % 2
+    real_dist = rolling_window(log_returns, windows[i], sparse = not (windows[i] == 1)).sum(axis=0).ravel()
+    fake_dist = rolling_window(y.T, windows[i], sparse = not (windows[i] == 1)).sum(axis=0).ravel()
+    axs[row, col].hist(np.array([real_dist, fake_dist], dtype='object'), bins=50, density=True)
+    axs[row,col].set_xlim(*np.quantile(fake_dist, [0.001, .999]))
+    
+    axs[row,col].set_title('{} day distribution'.format(windows[i]), size=16)
+    axs[row,col].yaxis.grid(True, alpha=0.5)
+    axs[row,col].set_xlabel('Cumalative log')
+    axs[row,col].set_ylabel('Frequency')
+
+axs[0,0].legend(['Historical', 'Synthetic'])
 plt.show()
 ```
 
